dc030a9256fe243b78226e7f5f167c4c084f1b49
Laura Long <lauralonggone@gmail.com>
2024-04-09 21:09:48 -0600

    Test random drink and home page

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,13 +1,29 @@
 describe('home page', () => {
-  it('passes', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
+
   it('should display a title and a quiz section', () => {
-    cy.visit('http://localhost:3000')
     cy.get('.home-title > h1').should('have.text', 'Mix Master')
     cy.get('.home-title > p').should('have.text', 'your drink recipe companion')
     cy.get('.quiz-title').should('have.text', 'Do you prefer...')
-    cy.get('.radio-container').should('have.text', 'Alcoholic beveragesNA beverages')
+    cy.get('.radio-container').should(
+      'have.text',
+      'Alcoholic beveragesNA beverages'
+    )
+    cy.get('.radio-container').find('input').should('have.length', 2)
     cy.get('button').should('have.text', 'Find Drinks')
+  })
+
+  it('should navigate to cocktails page when alc radio btn is selected', () => {
+    cy.get(':nth-child(1) > input').check()
+    cy.get('button').click()
+    cy.url().should('include', '/cocktails')
+  })
+
+  it('should navigate to the NA drinks page when the NA radio btn is selected', () => {
+    cy.get(':nth-child(2) > input').check()
+    cy.get('button').click()
+    cy.url().should('include', '/mocktails')
   })
 })
